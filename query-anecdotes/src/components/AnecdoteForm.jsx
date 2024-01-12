e89159dbe2deffc0caefa8e2bc63c033ff167669
FullStack-Tehtavien-Palautus <github-hy-fullstack@v.hix.fi>
2024-01-12 15:59:12 +0200

    Tehtävä 6.21

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,28 @@
-const AnecdoteForm = () => {
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from '../requests' 
 
-  const onCreate = (event) => {
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+ 
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
-}
+  }
 
   return (
     <div>
       <h3>create new</h3>
-      <form onSubmit={onCreate}>
+      <form onSubmit={addAnecdote}>
         <input name='anecdote' />
         <button type="submit">create</button>
       </form>
